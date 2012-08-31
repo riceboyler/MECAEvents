f6e2c64e916b8aa13e6221982b53498384df9535
Jason Clark <riceboyler@outlook.com>
2012-08-31 11:10:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MECAEvents::Application.config.secret_token = 'c4f8ce38931dfaea0619ef5a99bdd25f3579e27699dba2c3fb852025dc6885e83d170dab39e30248886f702deec084869d50a39e94d3c5643cd2638131347585'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MECAEvents::Application.config.session_store :cookie_store, key: '_MECAEvents_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MECAEvents::Application.config.session_store :active_record_store
